74c1fab6147d34753d7c6371c24da0e03a641ab7
David <seuemail@exemplo.com>
2025-11-20 14:27:00 -0300

    Iniciando projeto

---
+++ src/components/header/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import styles from './styles.module.scss'
+
+const Header = () => {
+  return (
+    <header> 
+        <h2>Header</h2>
+    </header>
+  )
+}
+
+export default Header
